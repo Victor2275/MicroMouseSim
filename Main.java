12282ec4f4a4da7d2ce87980200152df67e074e7
Victor2275 <gusev0219@gmail.com>
2026-01-01 19:47:57 -0800

    fixing jar

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;  
+
+
 
 public class Main {
-    
     public static void main(String[] args) throws InterruptedException {
 
         Maze maze = new Maze();
